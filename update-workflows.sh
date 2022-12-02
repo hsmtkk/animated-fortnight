e742c0401efeee2e816ece3239539297616aefb7
hsmtkk <hsmtkk@gmail.com>
2022-12-02 11:57:14 +0900

    fix option name

--- update-workflows.sh
+++ update-workflows.sh
@@ -2,7 +2,7 @@
 apt-get -y install jq
 randomgen_url=`gcloud functions describe randomgen --format json --gen2 --region asia-northeast1 | jq .serviceConfig.uri`
 multiply_url=`gcloud functions describe multiply --format json --gen2 --region asia-northeast1 | jq .serviceConfig.uri`
-sed -i 's|var_randomgen_url|${randomgen_url}|' workflows.yaml
-sed -i 's|var_multiply_url|${multiply_url}|' workflows.yaml
+sed -i "s|var_randomgen_url|${randomgen_url}|" workflows.yaml
+sed -i "s|var_multiply_url|${multiply_url}|" workflows.yaml
 cat workflows.yaml
-gcloud workflows deploy my-workflows --region asia-northeast1 --source workflows.yaml
+gcloud workflows deploy my-workflows --location asia-northeast1 --source workflows.yaml
